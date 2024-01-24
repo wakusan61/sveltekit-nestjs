4423b84655025bd676a766a152516c69709979e0
wakusan61 <wy.6126.yyy@gmail.com>
2024-01-24 15:02:46 +0900

    フォーマット

--- apps/my-web/src/routes/todo/edit/[no=number]/+page.server.ts
+++ apps/my-web/src/routes/todo/edit/[no=number]/+page.server.ts
@@ -1,23 +1,24 @@
-import { createRequestURL } from "$lib/define";
-import { apiClient } from "$lib/api-client";
-import type { PageServerLoad } from "./$types";
-import { schemas } from "$lib/generated-client"
-import type { z } from "zod";
+import { createRequestURL } from '$lib/define';
+import { apiClient } from '$lib/api-client';
+import type { PageServerLoad } from './$types';
+import { schemas } from '$lib/generated-client';
+import type { z } from 'zod';
 
 type PageData = {
-  todo: z.infer<typeof schemas.GetResponseDTO>
-}
+	todo: z.infer<typeof schemas.GetResponseDTO>;
+};
 
-export const load:PageServerLoad = async (serverLoad):Promise<PageData> => {
-  try {
-    console.log( Number(serverLoad.params.no))
-    const result = await apiClient.TodoController_get({params:{
-      // TODO: params/number.ts で number を保証しているが string になるためやむなく numberに変換
-      no: Number(serverLoad.params.no)
-    }})
-    return {todo:result}
-  } catch (error) {
-    console.error(error)
-    throw new Error("リクエストでエラーが発生しました。")
-  }
-}
+export const load: PageServerLoad = async (serverLoad): Promise<PageData> => {
+	try {
+		const result = await apiClient.TodoController_get({
+			params: {
+				// TODO: params/number.ts で number を保証しているが string になるためやむなく numberに変換
+				no: Number(serverLoad.params.no)
+			}
+		});
+		return { todo: result };
+	} catch (error) {
+		console.error(error);
+		throw new Error('リクエストでエラーが発生しました。');
+	}
+};
