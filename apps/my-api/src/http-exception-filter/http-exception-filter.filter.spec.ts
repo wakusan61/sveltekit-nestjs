92d1292df0a5b27d07e92bd09449ae8421c4b3bf
wakusan61 <wy.6126.yyy@gmail.com>
2024-01-26 22:41:19 +0900

    fix name

--- apps/my-api/src/http-exception-filter/http-exception-filter.filter.spec.ts
+++
@@ -1,7 +0,0 @@
-import { HttpExceptionFilterFilter } from './http-exception-filter.filter';
-
-describe('HttpExceptionFilterFilter', () => {
-  it('should be defined', () => {
-    expect(new HttpExceptionFilterFilter()).toBeDefined();
-  });
-});
